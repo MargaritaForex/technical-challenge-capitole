5811d3c90bd6c063bf4331da0dcd24ff207f863c
MargaritaForex <ingerika.forero@gmail.com>
2023-02-06 22:19:13 -0500

    chore: create project structure

---
+++ src/test/java/com/technical/challenge/CapitoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
